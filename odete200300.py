df2a2850226efcecbdb49865e16d08f68e9a012c
Joana004 <joanasimao361@gmail.com>
2026-03-11 09:15:58 +0000

    Add files via upload

---
+++ odete200300.py
@@ -0,0 +1,85 @@
+#nome="joana"
+#idade=22
+#peso=1.60
+#linda=True
+#Primeiro dia de aula
+
+
+#print(f"meu nome é ",{nome} ,"minha idade é", {idade},"meu peso",{peso},"linda",{linda})
+
+#nome1= input("Digite o meu nome :")
+#idade1=int(input("Digite a minha idade"))
+#peso1=float(input("digite o eu peso"))
+
+
+#print(f"meu nome é ",{nome1} ,"minha idade é", {idade1},"meu peso",{peso1})
+
+
+#n=int(input("digite um numero"))
+#n1=int(input("digite o segundo numero "))
+#n2=int(input("digite o terceiro numero"))
+#media=(n+n1+n2)/3
+#print(f"O primeiro numero eh ,{n} o segundo numero eh, {n1}  o terceiro numero eh,{n2} a media eh,{media}")
+
+#Escreva um programa que pessa dois numeros e mostra a somo,divisão,multiplicação,subritração
+
+#T1=int(input("digite o primeiro numero : "))
+#T2=int(input("digite o sengundo numero : "))
+
+#soma=T1+T2
+#div=T1/T2
+#mult=T1*T2
+#sub=T1-T2
+#print(f"o primeiro numero eh ,{T1} o segundo numero eh {T2} a soma eh ,{soma},divisao {div:1f} eh , multiplicação {mult} , eh subritação{sub} eh")
+
+#print(20**3)
+#print(20//3)
+#print(20%3)
+
+#r1=int(input("Primeiro numero: "))
+#r2=int(input("Segundo numero: "))
+#rd=r1%r2
+#print(f"O resto da divisao de dois numeros é: {rd}")
+
+#Tarefa do dia 10/03/2026
+#ESCREVA UM ALGORITMO QUE MOSTRA A SOMA DE DOIS NÚMEROS E MOSTRA O RESTO DA DIVISÃ
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+soma = n1 + n2
+resto = n1 % n2
+print("A soma é:", soma)
+print("O resto da divisão é:", resto)
+
+#CRIA UM ALGORITMO QUE PEDE DOIS NUMEROS FAZ SUBRITRAÇÃO,E MULTIPLICAÇÃO
+
+
+#a = int(input("Digite o primeiro número: "))
+#b = int(input("Digite o segundo número: "))
+#print("Subtração:", a - b)
+#print("Multiplicação:", a * b)
+
+#CRIA UM ALGORITMO QUE MULT 7 POR 8 E SUBRITRAIA 10.
+#a = 7
+#b = 8
+#c = a * b
+#resultado = c - 10
+#print(resultado)
+
+
+#DIVIDA 25 POR 4 E MOSTRE O QUOCIENTE E O RESTO
+#a = 25
+#b = 4
+
+#quociente = a // b
+#resto = a % b
+
+#print("Quociente:", quociente)
+#print("Resto:", resto)
+
+#CRIA UM ALGORITMO QUE ELEVE 3 A POTENCIA 4 .
+
+resultado = 3 ** 4
+print(resultado)
+
